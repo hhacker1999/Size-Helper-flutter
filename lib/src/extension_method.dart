03f644033ed30aa85330320d120d6ad7485ecc8b
hhacker1999 <harsh.sadh313@gmail.com>
2021-05-27 11:03:10 +0530

    Fix num not being treated as int or double

--- lib/src/extension_method.dart
+++ lib/src/extension_method.dart
@@ -1,7 +1,14 @@
 import 'size_helper.dart';
 
-extension SizeHelp on num {
-  num get h => SizeUtility.height(this);
-  num get w => SizeUtility.width(this);
-  num get f => SizeUtility.font(this);
+extension SizeHelpInt on int {
+  int get h => SizeUtility.height(this).toInt();
+  int get w => SizeUtility.width(this).toInt();
+  int get f => SizeUtility.font(this).toInt();
+}
+
+
+extension SizeHelpDouble on double {
+  double get h => SizeUtility.height(this).toDouble();
+  double get w => SizeUtility.width(this).toDouble();
+  double get f => SizeUtility.font(this).toDouble();
 }
