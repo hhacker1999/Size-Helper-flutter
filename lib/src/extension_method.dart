fb3b44288a10118ae553bc2f94241fd1e3c11b94
hhacker1999 <harsh.sadh313@gmail.com>
2021-05-27 11:06:11 +0530

    Remove extension method on int and double

--- lib/src/extension_method.dart
+++ lib/src/extension_method.dart
@@ -1,13 +1,8 @@
 import 'size_helper.dart';
 
-extension SizeHelpInt on int {
-  int get h => SizeUtility.height(this).toInt();
-  int get w => SizeUtility.width(this).toInt();
-  int get f => SizeUtility.font(this).toInt();
-}
 
 
-extension SizeHelpDouble on double {
+extension SizeHelpDouble on num {
   double get h => SizeUtility.height(this).toDouble();
   double get w => SizeUtility.width(this).toDouble();
   double get f => SizeUtility.font(this).toDouble();
